078ab0e948826431be1e4d5202f0c6ebd5924164
adamijak <jakub.adamik@greycorbel.com>
2024-04-24 10:22:18 +0200

    feat: add ToAsyncEnumerable

--- Cosmos/Extensions/FeedIteratorExtensions.cs
+++ Cosmos/Extensions/FeedIteratorExtensions.cs
@@ -5,6 +5,16 @@ namespace Adamijak.Cosmos.Extensions;
 
 public static class FeedIteratorExtensions
 {
+    public static async IAsyncEnumerable<ResponseMessage> ToAsyncEnumerable(this FeedIterator iterator,
+        [EnumeratorCancellation] CancellationToken cancelToken = default)
+    {
+        while (iterator.HasMoreResults)
+        {
+            var value = await iterator.ReadNextAsync(cancelToken);
+            yield return value;
+        }
+    }
+    
     public static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(this FeedIterator<T> iterator,
         [EnumeratorCancellation] CancellationToken cancelToken = default)
     {
